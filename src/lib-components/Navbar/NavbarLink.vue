ca38fd9e52786b31cb72a5b801518c0bab7b33fe
tnorthern <traynorthern@yahoo.com>
2022-01-14 12:11:39 -0600

    Navbar rename start

---
+++ src/lib-components/Navbar/NavbarLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <AppLink class="reset mega-nav__control js-mega-nav__control">{{ text }}</AppLink>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import AppLink from '../Link/AppLink.vue';
+export default defineComponent({
+    props: {
+        text: {
+            type: String,
+            default: '',
+        },
+    },
+    setup() {
+        return {};
+    },
+    components: { AppLink },
+});
+</script>
